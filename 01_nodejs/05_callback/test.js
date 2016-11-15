37d81fd7c6a5b1c916ba75ab79ec8f4ee1667b07
yasuhiro <yasuhiro@localhost.localdomain>
2016-11-16 06:06:44 +0900

    bbb

--- 01_nodejs/05_callback/test.js
+++ 01_nodejs/05_callback/test.js
@@ -1,18 +1,29 @@
 //**********************************************
-// callbackサンプル
+// callbackサンプル 
 //**********************************************
 function doSomething(callback){
   //関数を引数として渡すことができる。
   //渡す関数を変更することで振舞いを切替えることができる
   callback('Stuff ','goes ','here.');
 }
-
 function foo(a,b,c){
   console.log(a + b + c);
 }
 
-
 doSomething(foo);
 
+
+//**********************************************
+// callbackサンプル (引数も渡す)
+//**********************************************
+function foo2(a){
+  console.log('foo2 arg... ' + a);
+}
+function doSomething2(callback,callback_arg){
+  console.log('doSomething2 call');
+  callback(callback_arg);
+}
+
+doSomething2(foo2,'test_arg_desu');
 
 
