0ba1bd446200c9cb5b59093f67d88ddf245fd1c3
yasuhiro <yasuhiro@localhost.localdomain>
2016-12-21 06:14:35 +0900

    aaa

---
+++ 01_nodejs/02_global/02/global-a.js
@@ -0,0 +1,6 @@
+var b = require('./global-b.js');
+
+console.log(global.i);
+global.print();
+
+
---
+++ 01_nodejs/02_global/02/global-b.js
@@ -0,0 +1,6 @@
+global.i = 100;
+global.print = function(){
+  console.log('added by module b.');
+};
+
+
