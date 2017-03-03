574402ba78e8701edc2f3fa26db458429f39c2cd
test01aa <yasuhiro@124x35x64x209.ap124.ftth.ucom.ne.jp>
2017-03-04 06:24:05 +0900

    aaa

---
+++ 03_spring_fw/02/hajiboot-di_2.1.2/src/main/java/com/example/AppConfig.java
@@ -0,0 +1,20 @@
+package com.example;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import com.example.app.AddCalculator;
+import com.example.app.ArgumentResolver;
+import com.example.app.Calculator;
+import com.example.app.ScannerArgumentResolver;
+
+@Configuration
+public class AppConfig {
+	@Bean
+	Calculator calculator(){
+		return new AddCalculator();
+	}
+	@Bean
+	ArgumentResolver argumentResolver(){
+		return new ScannerArgumentResolver();
+	}
+}
