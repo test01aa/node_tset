9487efe4919c3fe50a82852682988c309e4f4292
test01aa <test01aa@example.com>
2017-03-06 06:06:32 +0900

    ccc

--- 03_spring_fw/02/hajiboot-di_2.1.3/src/main/java/com/example/App.java
+++ 03_spring_fw/02/hajiboot-di_2.1.3/src/main/java/com/example/App.java
@@ -1,10 +1,10 @@
 package com.example;
 
+import com.example.app.Frontend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Import;
-import com.example.app.Frontend;
 
 @EnableAutoConfiguration
 @Import(AppConfig.class)
@@ -12,8 +12,9 @@ public class App
 {
     public static void main( String[] args )
     {
-    	ConfigurableApplicationContext context = SpringApplication.run(App.class, args);
-    	Frontend frontend = context.getBean(Frontend.class);
-    	frontend.run();    	
+    	try (ConfigurableApplicationContext context = SpringApplication.run(App.class, args)){
+        	Frontend frontend = context.getBean(Frontend.class);
+        	frontend.run();    	    		
+    	}
     }
 }
