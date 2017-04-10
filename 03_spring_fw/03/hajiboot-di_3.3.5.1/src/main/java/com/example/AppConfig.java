8865c5e124390b6438d25ac1f2083c8b3b6132cf
test01aa <test01aa@example.com>
2017-04-11 06:01:21 +0900

    aa

--- 03_spring_fw/03/hajiboot-di_3.3.5.1/src/main/java/com/example/AppConfig.java
+++ 03_spring_fw/03/hajiboot-di_3.3.5.1/src/main/java/com/example/AppConfig.java
@@ -5,6 +5,9 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.web.filter.CharacterEncodingFilter;
 
 import javax.sql.DataSource;
 
@@ -23,6 +26,15 @@ public class AppConfig {
                 .password(this.properties.getPassword());
         this.dataSource = factory.build();
         return this.dataSource;
+    }
+    
+    @Order(Ordered.HIGHEST_PRECEDENCE)
+    @Bean
+    CharacterEncodingFilter characterEncodingFilter(){
+    	CharacterEncodingFilter filter = new CharacterEncodingFilter();
+    	filter.setEncoding("UTF-8");
+    	filter.setForceEncoding(true);
+    	return filter;
     }
 
 }
