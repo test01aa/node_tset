e7a4d6fecdc243f23632f59869c947c1ad541002
test01aa <test01aa@example.com>
2017-05-07 07:13:44 +0900

    aaa

--- 03_spring_fw/03/hajiboot-di_3.5/src/main/resources/data.sql
+++ 03_spring_fw/03/hajiboot-di_3.5/src/main/resources/data.sql
@@ -1,4 +1,7 @@
-INSERT INTO customers(first_name, last_name) VALUES('Nobita', 'Nobi');
-INSERT INTO customers(first_name, last_name) VALUES('Takeshi', 'Goda');
-INSERT INTO customers(first_name, last_name) VALUES('Suneo', 'Honekawa');
-INSERT INTO customers(first_name, last_name) VALUES('Shizuka', 'Minamoto');
+INSERT INTO customers (first_name, last_name) VALUES ('Nobita', 'Nobi');
+INSERT INTO customers (first_name, last_name) VALUES ('Takeshi', 'Goda');
+INSERT INTO customers (first_name, last_name) VALUES ('Suneo', 'Honekawa');
+INSERT INTO customers (first_name, last_name) VALUES ('Shizuka', 'Minamoto');
+
+INSERT INTO users (username, encoded_password) VALUES ('user1', '$2a$10$1gJJgBlL75OIjkSgkYPXI.mV7ihEPjxIiCkXKBEc7/r9xUIjZyc9i' /*demo*/);
+INSERT INTO users (username, encoded_password) VALUES ('user2', '$2a$10$1gJJgBlL75OIjkSgkYPXI.mV7ihEPjxIiCkXKBEc7/r9xUIjZyc9i' /*demo*/);
