e641d7568848af85f0b34ccd8402c826b0438f9f
yasuhiro <yasuhiro@124x35x64x76.ap124.ftth.ucom.ne.jp>
2018-05-07 05:31:05 +0900

    a

---
+++ 07_chainer/01/pydlshogi/network/policy.py
@@ -0,0 +1,8 @@
+from chainer import Chain
+import chainer.functions as F
+import chainer.links     as L
+
+from python-dlshogi.common import *
+
+
+
--- 07_chainer/01/train_mnist.py
+++ 07_chainer/01/train_mnist.py
@@ -61,6 +61,99 @@ if args.gpu >= 0:
     chainer.cuda.get_device_from_id(args.gpu).use()
     model.to_gpu()
 
+#####################################################################
+####   勾配下降法の手法としてSGDを使用
+#####################################################################
+optimizer = chainer.optimizers.SGD()
+optimizer.setup(model)
+
+#####################################################################
+####  保存したモデルを読み込み 
+#####################################################################
+#  保存したモデルを読み込み 
+if args.initmodel:
+    print('Load model from' , args.initmodel)
+    serializers.load_npz(args.initmodel, model)
+ 
+# 保存した最適化状態を復元
+if args.resume:
+    print('Load optimizer state from', args.resume)
+    serializers.load_npz(args.resume, optimizer)
+
+#####################################################################
+####  MNISTデータセット読み込み 
+#####################################################################
+train, test = chainer.datasets.get_mnist()
+train_iter  = chainer.iterators.SerialIterator(train, args.batchsize               )
+test_iter   = chainer.iterators.SerialIterator(test , args.batchsize, shuffle=False)
+
+
+#####################################################################
+#### 学習ループ
+#####################################################################
+for epoch in range(1, args.epoch + 1):
+    # ミニバッチ単位で学習
+    sum_loss = 0
+    itr = 0
+    for i in range(0, len(train), args.batchsize):
+        # ミニバッチデータ
+        train_batch = train_iter.next()
+        x, t = chainer.dataset.concat_examples(train_batch, args.gpu)
+
+        # 順伝播
+        y = model(x)
+
+        # 勾配を初期化
+        model.cleargrads()
+
+        # 損失計算
+        loss = F.softmax_cross_entropy(y, t)
+
+        # 誤差逆伝播
+        loss.backward()
+        optimizer.update()
+
+        sum_loss += loss.data
+        itr += 1
+
+    # 評価
+    sum_test_loss = 0
+    sum_test_accuracy = 0
+    test_itr = 0
+    for i in range(0, len(test), args.batchsize):
+        # ミニバッチデータ
+        test_batch = test_iter.next()
+        x_test, t_test = chainer.dataset.concat_examples(test_batch, args.gpu)
+
+        # 順伝播
+        y_test = model(x_test)
+        # 損失計算
+        sum_test_loss += F.softmax_cross_entropy(y_test, t_test).data
+        # 一致率計算
+        sum_test_accuracy += F.accuracy(y_test, t_test).data
+        test_itr += 1
+
+    print('epoch={}, train loss={}, test loss={}, accuracy={}'.format(optimizer.epoch + 1, sum_loss / itr, sum_test_loss / test_itr, sum_test_accuracy / test_itr))
+
+    optimizer.new_epoch()
+
+
+#####################################################################
+####  モデル保存
+#####################################################################
+# モデル保存
+print('save the model')
+serializers.save_npz('mlp.model', model)
+
+# 最適化状態保存
+print('save the optimizer')
+serializers.save_npz('mlp.state', optimizer)
+
+
+
+
+
+
 
 
 
