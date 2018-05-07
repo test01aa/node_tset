4c9a80814094219cf5c2d829021535529207d13f
test01aa <test01aa@example.com>
2018-05-08 06:08:26 +0900

    aaa

--- 07_chainer/01/pydlshogi/network/policy.py
+++ 07_chainer/01/pydlshogi/network/policy.py
@@ -1,4 +1,4 @@
-from chainer import Chain
+from   chainer import Chain
 import chainer.functions as F
 import chainer.links     as L
 
@@ -9,19 +9,19 @@ class PolicyNetwork(Chain):
     def __init__(self):
         super(PolicyNetwork, self).__init__()
         with self.init_scope():
-            self.l01 = Convolution2D(in_channels = 104, out_channels = ch, ksize = 3, pad = 1)
-            self.l02 = Convolution2D(in_channels = ch , out_channels = ch, ksize = 3, pad = 1)
-            self.l03 = Convolution2D(in_channels = ch , out_channels = ch, ksize = 3, pad = 1)
-            self.l04 = Convolution2D(in_channels = ch , out_channels = ch, ksize = 3, pad = 1)
-            self.l05 = Convolution2D(in_channels = ch , out_channels = ch, ksize = 3, pad = 1)
-            self.l06 = Convolution2D(in_channels = ch , out_channels = ch, ksize = 3, pad = 1)
-            self.l07 = Convolution2D(in_channels = ch , out_channels = ch, ksize = 3, pad = 1)
-            self.l08 = Convolution2D(in_channels = ch , out_channels = ch, ksize = 3, pad = 1)
-            self.l09 = Convolution2D(in_channels = ch , out_channels = ch, ksize = 3, pad = 1)
-            self.l10 = Convolution2D(in_channels = ch , out_channels = ch, ksize = 3, pad = 1)
-            self.l11 = Convolution2D(in_channels = ch , out_channels = ch, ksize = 3, pad = 1)
-            self.l12 = Convolution2D(in_channels = ch , out_channels = ch, ksize = 3, pad = 1)
-            self.l13 = Convolution2D(in_channels = ch , out_channels = MOVE_DIRECTION_LABEL_NUM , ksize = 1, nobias = True)
+            self.l01 = L.Convolution2D(in_channels = 104, out_channels = ch, ksize = 3, pad = 1)
+            self.l02 = L.Convolution2D(in_channels = ch , out_channels = ch, ksize = 3, pad = 1)
+            self.l03 = L.Convolution2D(in_channels = ch , out_channels = ch, ksize = 3, pad = 1)
+            self.l04 = L.Convolution2D(in_channels = ch , out_channels = ch, ksize = 3, pad = 1)
+            self.l05 = L.Convolution2D(in_channels = ch , out_channels = ch, ksize = 3, pad = 1)
+            self.l06 = L.Convolution2D(in_channels = ch , out_channels = ch, ksize = 3, pad = 1)
+            self.l07 = L.Convolution2D(in_channels = ch , out_channels = ch, ksize = 3, pad = 1)
+            self.l08 = L.Convolution2D(in_channels = ch , out_channels = ch, ksize = 3, pad = 1)
+            self.l09 = L.Convolution2D(in_channels = ch , out_channels = ch, ksize = 3, pad = 1)
+            self.l10 = L.Convolution2D(in_channels = ch , out_channels = ch, ksize = 3, pad = 1)
+            self.l11 = L.Convolution2D(in_channels = ch , out_channels = ch, ksize = 3, pad = 1)
+            self.l12 = L.Convolution2D(in_channels = ch , out_channels = ch, ksize = 3, pad = 1)
+            self.l13 = L.Convolution2D(in_channels = ch , out_channels = MOVE_DIRECTION_LABEL_NUM , ksize = 1, nobias = True)
             self.l13_bias = L.Bias(shape=(9*9*MOVE_DIRECTION_LABEL_NUM))
 
     def __call__(self, x):
@@ -39,8 +39,4 @@ class PolicyNetwork(Chain):
         h12 = F.relu(self.l12(h11))
         h13 = self.l13(h12) 
         return self.l13_bias(F.reshape(h13,(-1,9*9*MOVE_DIRECTION_LABEL_NUM)))
-
-
-
-
 
